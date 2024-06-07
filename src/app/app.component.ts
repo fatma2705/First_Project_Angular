638fd3bca45f477670d028926cc77fd2a229e6fa
fatma2705 <arafatma2003@gmail.com>
2024-06-07 17:20:30 +0200

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeroesComponent,MessagesComponent,RouterLink],
+  imports: [RouterOutlet,HeroesComponent,MessagesComponent,RouterLink,HttpClientInMemoryWebApiModule,InMemoryDataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,17 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable,of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+    ) { }
 
   getHeroes(): Observable<Hero[]>{
     const heroes = of(HEROES);
@@ -23,6 +27,10 @@ export class HeroService {
   const hero = HEROES.find(h => h.id === id)!;
   this.messageService.add(`HeroService: fetched hero id=${id}`);
   return of(hero);
+}
+
+private log(message: string) {
+  this.messageService.add('HeroService: ${message}');
 }
 
 }
